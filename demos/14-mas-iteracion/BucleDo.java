0f86edcda96fa63adfc04d811c83556a931ff0a2
Alejandro Cerezo <alce65@hotmail.es>
2025-06-17 12:16:29 +0200

    Add do while demo

---
+++ demos/14-mas-iteracion/BucleDo.java
@@ -0,0 +1,14 @@
+
+public class BucleDo {
+
+    public static void main(String[] args) {
+        
+
+        boolean flag = false;
+
+        do {
+            System.out.println("Estoy dentro del bucle"); 
+        } while (flag);
+
+    }
+}
