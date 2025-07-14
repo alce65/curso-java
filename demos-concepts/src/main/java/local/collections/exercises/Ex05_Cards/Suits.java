c02053e06be704b6e01f0ea16fe412dd49a8659e
Alejandro Cerezo <alce65@hotmail.es>
2025-07-14 11:41:19 +0200

    Solve collections exercise: Cards

---
+++ demos-concepts/src/main/java/local/collections/exercises/Ex05_Cards/Suits.java
@@ -0,0 +1,13 @@
+package local.collections.exercises.Ex05_Cards;
+
+public enum Suits {
+    HEARTS("Corazones","♥"), SPADES("Picas", "♠"), DIAMONDS("Diamantes", "♦"), CLUBS("Tréboles", "♣");
+
+    String label;
+    String icon;
+
+    Suits(String label,String icon) {
+        this.label = label;
+        this.icon = icon;
+    }
+}
